75bb0d0beaad79d124922fd135ef8f75b21f94cb
unknown <Rav5385@gmail.com>
2022-07-22 20:46:39 +0300

    небольшие правки к дз4

--- SEMINAR4/Program.cs
+++ SEMINAR4/Program.cs
@@ -70,7 +70,7 @@ void Zadacha29()
     {
         if (series[i] != ' ') 
         {
-        seriesNew += series[i];
+            seriesNew += series[i];
         }
     }
     return seriesNew;
@@ -89,7 +89,7 @@ void Zadacha29()
         }
     }
 
-    // функция  создания и заполнения массива из строки
+    // заполняем массив
     int[] ArrayOfNumbers(string seriesNew)
     { 
 
@@ -118,19 +118,19 @@ void Zadacha29()
     // функция  вывода массива на печать 
     void PrintArry(int[] coll)
     {
-    int count = coll.Length;
-    int index = 0;
-    Console.Write("[");
-    while(index < count)
-    {
-        Console.Write(coll[index]);
-        index++;
-        if (index < count)
+        int count = coll.Length;
+        int index = 0;
+        Console.Write("[");
+        while(index < count)
         {
-        Console.Write(",");
+            Console.Write(coll[index]);
+            index++;
+            if (index < count)
+            {
+            Console.Write(",");
+            }
         }
-    }
-    Console.Write("]");
+        Console.Write("]");
     } 
     
     string seriesNew = RemovingSpaces(seriesOfNumbers);
@@ -141,4 +141,4 @@ void Zadacha29()
 
 //Zadacha25();
 //Zadacha27();
-Zadacha29();
+//Zadacha29();
