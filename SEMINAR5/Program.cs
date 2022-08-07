a5ede81486526490e5a8fc665e75a0253db8d1aa
Rav53 <Rav5385@gmail.com>
2022-08-07 21:58:04 +0300

    закодил задачу 38=)

--- SEMINAR5/Program.cs
+++ SEMINAR5/Program.cs
@@ -21,6 +21,8 @@
     Console.WriteLine($"Чётных чисел:" + chetnum);
 }
 
+//--------------------------------------------------------------
+
 void Zadacha36()
 {
 //Задача 36: Задайте одномерный массив,
@@ -43,8 +45,59 @@ void Zadacha36()
         Console.WriteLine($"Сумма чисел в нечётных позициях = " + sum);
 }
 
-Zadacha34();
+//--------------------------------------------------------------
+void Zadacha38()
+{
+    //Задайте массив вещественных чисел. 
+    //Найдите разницу между максимальным 
+    //и минимальным элементов массива.
+    int size = 4;
+    Double[] array = new Double[size];
+    FillArray(array);
+    PrintArray(array);
+    Double min = array[0];
+    Double max = array[0];
+
+    for (int i = 0; i < size; i++)
+    {
+      if (max < array[i])
+      {
+        max = array[i];
+      }
+      if (min > array[i])
+      {
+        min = array[i];
+      }
+    }
+    Console.WriteLine($"Разница между max ({max}) и min ({min}) значниями = {max - min}");
+
+
+void FillArray(Double[] array)
+{
+    int size = array.Length;
+    Random random = new Random();
+    for (int i = 0; i < size; i++)
+    {
+      array[i] = Convert.ToDouble(new Random().Next(1,11)) / 10;
+    }
+}
+
+void PrintArray(Double[] array)
+{
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+      Console.Write(array[i] + "  ");
+    }
+    Console.WriteLine();
+}    
+
+//--------------------------------------------------------------
+
+}
+//Zadacha34();
 //Zadacha36();
+Zadacha38();
 
 void FillArray(int[] array)
 {
