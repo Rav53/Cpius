ecf62cb9475692b4718c410fd03f5021a5dbbb6e
Rav53 <Rav5385@gmail.com>
2022-08-07 13:54:37 +0300

    закодил задачу 36=)

--- SEMINAR5/Program.cs
+++ SEMINAR5/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿void Zadacha34()
+{
 
     //Задача 34: Задайте массив заполненный случайными
     //положительными трёхзначными числами. Напишите
@@ -18,10 +19,32 @@
         }
     }
     Console.WriteLine($"Чётных чисел:" + chetnum);
+}
 
+void Zadacha36()
+{
+//Задача 36: Задайте одномерный массив,
+// заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
 
-//Zadacha34();
+    int size = 4;
+    int[] array = new int[size];
+    FillArray(array);
+    PrintArray(array);
 
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if(i % 2 == 0) 
+        {
+        sum = sum + array[i];
+        }
+    }
+        Console.WriteLine($"Сумма чисел в нечётных позициях = " + sum);
+}
+
+Zadacha34();
+//Zadacha36();
 
 void FillArray(int[] array)
 {
@@ -29,12 +52,13 @@ void FillArray(int[] array)
     Random random = new Random();
     for (int i = 0; i < size; i++)
     {
-      array[i] = random.Next(100, 999);
+      array[i] = random.Next(100,1000);
     }
 }
 
 void PrintArray(int[] array)
 {
+    int size = array.Length;
     for (int i = 0; i < size; i++)
     {
       Console.Write(array[i] + "  ");
