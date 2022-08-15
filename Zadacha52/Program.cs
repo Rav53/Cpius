c6127d34fbc6be88af27cbd96a89232da7dd9171
Rav53 <Rav5385@gmail.com>
2022-08-16 00:02:27 +0300

    задача 52

---
+++ Zadacha52/Program.cs
@@ -0,0 +1,55 @@
+﻿void Zadacha52()
+{
+    //Задайте двумерный массив из целых чисел.
+    //Найдите среднее арифметическое элементов в каждом столбце.
+
+    Random random = new Random();
+    int rows = random.Next(4, 5);
+    int columns = random.Next(4, 5);
+    int[,] array = new int[rows, columns];
+    FillArray(array);
+    PrintArray(array);
+    
+    for (int j = 0; j < columns; j++)
+    {
+        double Sum = 0;
+        for (int i = 0; i < rows; i++)
+        {
+            Sum += array[i,j];
+            
+        }
+            Console.WriteLine($"Среднее арифметическое столбцов:{Sum / array.GetLength(1)}");
+    }
+
+
+void FillArray(int[,] array, int startNum = 0, int finishNum = 10)
+{
+    finishNum++;
+    Random random = new Random();
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = random.Next(startNum,finishNum);
+        }
+    }
+
+}
+
+void PrintArray(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+}
+Zadacha52();
