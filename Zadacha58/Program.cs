b546948548e3327ba6ff66c68c81abe8d66d81b9
Rav53 <Rav5385@gmail.com>
2022-08-18 19:37:18 +0300

    задача 58

--- Zadacha58/Program.cs
+++ Zadacha58/Program.cs
@@ -18,18 +18,18 @@
             for (int i = 0; i < n; i++)
                 for (int j = 0; j < n; j++)
                     result[i, j] = rand.Next(n, n);
-            for (int currentChar = 1, step = 0; step < n / 2; step++)
+            for (int currentIndex = 1, step = 0; step < n / 2; step++)
             {
                 for (int j = step; j < n - step; j++)
-                    result[step, j] = currentChar;
+                    result[step, j] = currentIndex;
                 for (int j = step; j < n - step; j++)
-                    result[n - step - 1, j] = currentChar;
+                    result[n - step - 1, j] = currentIndex;
                 for (int i = step + 2; i < n - step - 1; i++)
-                    result[i, step] = currentChar;
+                    result[i, step] = currentIndex;
                 for (int i = step + 1; i < n - step - 1; i++)
-                    result[i, n - step - 1] = currentChar;
-                currentChar = 1 - currentChar;
-                result[step + 1, step] = currentChar;
+                    result[i, n - step - 1] = currentIndex;
+                currentIndex = 1 - currentIndex;
+                result[step + 1, step] = currentIndex;
             }
             if (n % 2 != 0 && result[0, 0] == 1)
                 result[n / 2, n / 2] = 1;
