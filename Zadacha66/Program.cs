604293fc7689da68621f5aadc4f0c5f7368726a6
Rav53 <Rav5385@gmail.com>
2022-08-26 21:18:16 +0300

    задача 66

--- Zadacha66/Program.cs
+++ Zadacha66/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+//Задайте значения M и N. Напишите рекурсивный метод,
+//который найдёт сумму натуральных элементов в промежутке от M до N.
+Console.Write("Введи N: ");
+int N = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи M: ");
+int M = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine(SumNormalNumbers(N,M,0));
+
+int SumNormalNumbers(int firstNumber, int secondNumber, int sum)
+    {
+        if (firstNumber != secondNumber)
+        {
+            sum += firstNumber++;
+            sum = SumNormalNumbers(firstNumber, secondNumber, sum);
+            return sum;
+        }
+        else
+        {
+            sum += firstNumber;
+            return sum;
+        }
+    }
